2c69429aa2850275217faf6293d30d3b057cc5bb
Abhirami Rajesh <97380115+AbhiramiRS@users.noreply.github.com>
2022-03-23 12:06:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class 2D
+{
+public static void main(String[]args)
+{
+int a[ ]=new int[5];
+int i;
+Scanner s=new Scanner(System.in);
+System.out.println("enter array elements:");
+for( i=0;i<5;i++)
+{
+  a[i]=s.nextInt( );
+}
+System.out.println("array elements are");
+{
+for( i=0;i<5;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+}
