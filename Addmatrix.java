ba163ffb99cceafcb0abda7df2cf04d8c1526f5c
Abhirami Rajesh <97380115+AbhiramiRS@users.noreply.github.com>
2022-07-13 21:41:24 +0530

    Update Addmatrix.java

--- Addmatrix.java
+++ Addmatrix.java
@@ -1,55 +1,40 @@
 import java.util.*;
-class Addmatrix
+class MatrixAddition
 {
-public static void main(String[]args)
-{
-int i,j,n,m;
-Scanner s=new Scanner(System.in);
-
-System.out.println("Enter the number of rows and columns of matrix");
-    m = s.nextInt();
-    n = s.nextInt();
-
-    int a[][] = new int[m][n];
-    int b[][] = new int[m][n];
-    int sum[][] = new int[m][n];
-System.out.println("enter elements of first array:");
-for( i=0;i<m;i++)
-{
- for(j=0;j<n;j++)
-{
-  a[i][j]=s.nextInt( );
-}
-}
-System.out.println("enter elements of second array:");
-{
-for( i=0;i<m;i++)
-{
-for(j=0;j<n;j++)
-{
-b[i][j]=s.nextInt( );
-}
-}
-}
-System.out.println("matrix addition");
-{
- for (i= 0; i < m; i++)
-{
-      for (j= 0; j < n; j++)
-{  
-      sum[i][j] = a[i][j] + b[i][j];
-}
-}
-}
-System.out.println( "  product is ");
-{
-for(i=0;i<2;i++)
-{
-for(j=0;j<2;j++)
-{
-System.out.println(sum[i][j]+" ");
-}
-}
-}
-}
+  public static void main(String[]args)
+  {
+     int row, col, i, j;
+     Scanner S=new Scanner(System.in);
+     System.out.println("Enter the number of rows");
+     row = S.nextInt();
+     System.out.println("Enter the number of columns");
+     col = S.nextInt();
+     int mat1[][] = new  int[row][col];
+     int mat2[][] = new  int[row][col];
+     int res[][] = new  int[row][col];
+     System.out.println("Enter the elements of matrix1");
+     for( i=0 ; i< row ; i++)
+     {
+        for(j=0 ; j<col ; j++)
+        mat1[i][j] = S.nextInt();
+        System.out.println();
+     }
+     System.out.println("Enter the elements of matrix2");
+      for( i=0 ; i< row ; i++)
+     {
+        for(j=0 ;  j < col ; j++)
+        mat2[i][j] = S.nextInt();
+        System.out.println();
+     }
+     for(i=0 ; i < row ; i++)
+     for(j=0 ; j < row ; j++)
+     res[i][j] = mat1[i][j] + mat2[i][j];
+     System.out.println(" Sum of matrices:-  ");
+     for(i = 0 ; i < row ; i++)
+     {
+         for(j = 0 ; j < col ; j++)
+         System.out.print(res[i][j]+"\t");
+         System.out.println();
+     }
+  }
 }
